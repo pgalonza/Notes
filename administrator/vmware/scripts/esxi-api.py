fe09a34e4305482e81e6d1d86af2b56ea4fd08d8
Galonza Peter <petewindows2011@yandex.ru>
2018-12-24 17:59:27 +0300

    add connector

--- administrator/vmware/scripts/esxi-api.py
+++ administrator/vmware/scripts/esxi-api.py
@@ -7,38 +7,60 @@ import time
 import pyVmomi
 from pyVim.connect import SmartConnect, Disconnect
 
+_vmware_host = '***REMOVED***'
+_vmeare_user = '***REMOVED***'
+_vmware_password = '***REMOVED***'
+#_vm_not_suspend = ('co_app1', 'co_app2', 'co_app3', 'co_app4', 'co_db1', 'co_db2', 'co_db3', 'co_db4', 'co_srv_dev')
+_vm_suspend = ('co_device1', 'co_elasticsearch1', 'co_helpdesk2', 'co_kibana1', 'co_logstash1', 'co_proxy1', 'Server_test', 'co_test1', 'co_helpdesk1', 'co_rusguard1', 'co_test_win7', 'co_srv_av')
+#_vm_suspend = ('co_device1')
 
 def main():
-
-    vmNotSuspend = ('co_app1', 'co_app2', 'co_app3', 'co_app4', 'co_db1', 'co_db2', 'co_db3', 'co_db4', 'co_srv_dev')
-    context = None
-    if hasattr(ssl, '_create_unverified_context'):
-        context = ssl._create_unverified_context()
-        my_cluster = SmartConnect(host='***REMOVED***', user='***REMOVED***', pwd='***REMOVED***', port=443, sslContext=context)
-    if not my_cluster:
-        print("Could not connect to the specified host using specified "
-              "username and password")
-        return -1
+    my_cluster = vmware_connector()
 
     datacenter = my_cluster.content.rootFolder.childEntity[0]
     vms = datacenter.vmFolder.childEntity
 
     for vm in vms:
-        if vm.config.name in vmNotSuspend:
+        """
+        if vm.config.name in vm_not_suspend:
+            continue
+        """
+        if vm.config.name not in _vm_suspend:
             continue
 
-        print("Suspend:        " + vm.config.name)
-        #print("Virtual Machine Name:        " + vm.config.name)
-        #print("Virtual Machine OS:          " + vm.config.guestFullName)
-        #print("Virtual Machine UUID:        " + vm.config.uuid)
-        #print("")
-        #i.PowerOffVM_Task()
-        #i.SuspendVM_Task()
+        if vm.summary.runtime.powerState != 'poweredOn':
+            continue
+        elif vm.summary.runtime.powerState == 'poweredOn' and vm.summary.guest.toolsStatus != 'toolsOk':
+            vm.SuspendVM_Task()
+            print("Suspend:        " + vm.config.name)
+        else:
+            vm.ShutdownGuest()
+            print("Poweroff:        " + vm.config.name)
 
-    #datacenter.ShutdownHost_Task()
-    #time.sleep(10)
+        #print(vm.summary.runtime.powerState)
+        #print(vm.summary.guest.toolsStatus)
+        # print(vm.config)
+        # print("Virtual Machine Name:        " + vm.config.name)
+        # print("Virtual Machine OS:          " + vm.config.guestFullName)
+        # print("Virtual Machine UUID:        " + vm.config.uuid)
+        # print("")
+        # !vm.PowerOffVM_Task()!#
+        # !vm.SuspendVM_Task()!
+        # !vm.ShutdownGuest()!#
+
+    # !datacenter.ShutdownHost_Task()!
+    # time.sleep(15)
     print("PowerOff: " + str(datacenter))
     Disconnect(my_cluster)
+
+
+def vmware_connector():
+    if hasattr(ssl, '_create_unverified_context'):
+        context = ssl._create_unverified_context()
+        my_cluster = SmartConnect(host=_vmware_host, user=_vmeare_user, pwd=_vmware_password, port=443,
+                                  sslContext=context)
+
+    return my_cluster
 
 
 if __name__ == "__main__":
