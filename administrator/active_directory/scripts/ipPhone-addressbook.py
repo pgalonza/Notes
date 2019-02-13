81b3d561df94b97b03d2fcc0061d8e969bb4e9b5
Galonza Peter <petewindows2011@yandex.ru>
2019-02-13 12:10:30 +0300

    remove the dividers in the number, add a function to form a shared phone book

--- administrator/active_directory/scripts/ipPhone-addressbook.py
+++ administrator/active_directory/scripts/ipPhone-addressbook.py
@@ -8,9 +8,12 @@ import xml.etree.ElementTree as xml
 
 def main():
     for ou, origin in lm_auth.ad_ou_tree.items():
-        user_list = get_information(origin, ou)
+        if ou == 'all':
+            continue
+        user_list = get_information(origin[0], origin[1])
         path_to_file = ou + '.xml'
-        create_xml_file(user_list, path_to_file, ou)
+        create_xml_file(user_list, path_to_file, origin[1])
+    get_all_information()
 
 
 def get_information(origin, group_name):
@@ -24,34 +27,56 @@ def get_information(origin, group_name):
     user_list = {}
 
     for entry in conn.entries:
-        user_list[str(entry.displayName)] = [entry.ipPhone, group_name]
+        user_list[str(entry.displayName)] = [str(entry.ipPhone).replace('-', ''), group_name]
 
-    if not group_name == 'all' | 'co':
-        conn.search(origin,
+    if not group_name == 'Все' and not group_name == 'ЦУ':
+        conn.search('ou=co,dc=corp,dc=***REMOVED***,dc=ru',
                     '(&(objectCategory=person)(displayName=*)(givenName=*)(sn=*)(ipPhone=*)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))',
                     SUBTREE,
                     attributes=['ipPhone', 'displayName'])
 
         for entry in conn.entries:
-            user_list[str(entry.displayName)] = [entry.ipPhone, group_name]
+            user_list[str(entry.displayName)] = [str(entry.ipPhone).replace('-', ''), 'ЦУ']
 
     conn.unbind()
 
     return user_list
 
 
+def get_all_information():
+    conn = lm_auth.active_derectory_connector()
+    ou_list = ''
+    user_list = ''
+
+    for ou, origin in lm_auth.ad_ou_tree.items():
+        if ou == 'all':
+            continue
+        conn.search(origin[0],
+                    '(&(objectCategory=person)(displayName=*)(givenName=*)(sn=*)(ipPhone=*)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))',
+                    SUBTREE,
+                    attributes=['ipPhone', 'displayName'])
+        ou_list += '   <group display_name=\"{}\" />\n'.format(origin[1])
+        for entry in conn.entries:
+            user_list += "  <contact display_name=\"{}\" office_number=\"{}\" mobile_number=\"\" other_number=\"\" line=\"1\" " \
+                         "ring=\"\" group_id_name=\"{}\" />\n".format(entry.displayName, str(entry.ipPhone).replace('-', ''), origin[1])
+
+    return create_all_xml_file(ou_list, user_list)
+
+
 def create_xml_file(user_info, file_name, group_name):
     line = ''
     line += '''<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<root_group>
-    <group display_name=\"{}\" />
-</root_group>
-<root_contact>\n'''.format(group_name)
+<root_group>\n'''
+
+    line += '   <group display_name=\"{}\" />\n'.format(group_name)
+    if not group_name == 'Все' and not group_name == 'ЦУ':
+        line += '   <group display_name=\"ЦУ\" />\n'
+    line += '''</root_group>
+<root_contact>\n'''
 
     for name, number in user_info.items():
-        print(name, 'test')
-        line += "   <contact display_name=\"{}\" office_number=\"{}\" mobile_number=\"\" other_number=\"\" line=\"1\" "\
-                "ring=\"\" group_id_name=\"{}\" />\n".format(name, number, group_name)
+        line += "   <contact display_name=\"{}\" office_number=\"{}\" mobile_number=\"\" other_number=\"\" line=\"1\" " \
+                "ring=\"\" group_id_name=\"{}\" />\n".format(name, number[0], number[1])
 
     line += "</root_contact>"
 
@@ -59,13 +84,17 @@ def create_xml_file(user_info, file_name, group_name):
         index_file.write(line)
 
 
-def xml_structure_file(position, file_name):
-    _contact_header = '''<?xml version=\"1.0\" encoding=\"utf-8\"?>
-        <root_group>
-             <group display_name=\"Центральное управление\" />
-        </root_group>
-        <root_contact>\n'''
-    _contact_bottom = "</root_contact>"
+def create_all_xml_file(ou_list, user_list):
+    line = ''
+    line += '''<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<root_group>\n'''
+    line += ou_list
+    line += '''</root_group>
+<root_contact>\n'''
+    line += user_list
+    line += "</root_contact>"
+    with open('all.xml', 'w') as index_file:
+        index_file.write(line)
 
 
 if __name__ == "__main__":
