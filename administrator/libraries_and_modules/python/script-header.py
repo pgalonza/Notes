1e93b46307234f752bbeb5899cd20a88e343e45e
Galonza Peter <petewindows2011@yandex.ru>
2019-02-15 17:39:58 +0300

    script header

---
+++ administrator/libraries_and_modules/python/script-header.py
@@ -0,0 +1,8 @@
+__author__ = '{author}'
+__copyright__ = 'Copyright {year}, {project_name}'
+__credits__ = ['{credit_list}']
+__license__ = '{license}'
+__version__ = '{mayor}.{minor}.{rel}'
+__maintainer__ = '{maintainer}'
+__email__ = '{contact_email}'
+__status__ = '{dev_status}'
