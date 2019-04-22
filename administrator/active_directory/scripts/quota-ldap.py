71d1d85f8e63d8a2ba8b8e00401ce0383b55f476
Galonza Peter <petewindows2011@yandex.ru>
2019-04-22 17:30:02 +0300

    rewrite using lm_auth

--- administrator/active_directory/scripts/quota-ldap.py
+++ administrator/active_directory/scripts/quota-ldap.py
@@ -3,46 +3,35 @@
 import json
 import sys
 from ldap3 import Connection, Server, SUBTREE
-
-_ad_server = ''
-_ad_user = ''
-_ad_password = ''
-_ad_search_tree = 'dc=,dc=,dc='
-_ad_ou = ('CO')
+import lm_auth
 
 
 def main():
-    active_directory()
+    for ou, origin in lm_auth.ad_ou_tree.items():
+        if ou == 'all':
+            continue
+        get_information(origin[0], origin[1])
 
 
-def active_directory_connector():
-    server = Server(_ad_server)
-    conn = Connection(server, user=_ad_user, password=_ad_password)
-    conn.bind()
-    return conn
+def get_information(origin, group_name):
+    connection = lm_auth.active_derectory_connector()
 
+    connection.search(origin,
+                '(&(mail=*)(objectCategory=person)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))',
+                SUBTREE, attributes=['sAMAccountName', 'division', 'mail'])
 
-def active_directory():
-    conn = active_directory_connector()
-    print(conn)
-    for ou in _ad_ou:
-        _ad_search_tree = 'ou='+ou+',dc=,dc=,dc='
-        conn.search(_ad_search_tree,
-                    '(&(mail=*)(objectCategory=person)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))',
-                    SUBTREE, attributes=['sAMAccountName', 'division', 'mail'])
-
-        print('')
-        print(ou)
-        print('++++++++++++++++')
-        for entry in conn.entries:
-            # print(entry)
-            dn = (json.loads(entry.entry_to_json())['dn'])
-            login = entry.sAMAccountName
-            division = entry.division
-            mail = entry.mail
-            if division != "2G":
-                print(login, mail, division)
-    conn.unbind()
+    print('')
+    print(group_name)
+    print('++++++++++++++++')
+    for entry in connection.entries:
+        # print(entry)
+        #dn = (json.loads(entry.entry_to_json())['dn'])
+        login = entry.sAMAccountName
+        division = entry.division
+        mail = entry.mail
+        if division != "2G" and division != "2g":
+            print(login, mail, division)
+    connection.unbind()
 
 
 if __name__ == "__main__":
