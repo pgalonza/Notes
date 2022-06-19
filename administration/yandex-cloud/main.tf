af321442694cc239c2c92d7e55224a4468f59f90
Galonza Peter <p.galonza@evaron.ru>
2022-06-19 19:54:21 +0300

    add yandex_compute_instance_group

--- administration/yandex-cloud/main.tf
+++ administration/yandex-cloud/main.tf
@@ -8,15 +8,75 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "<token>"
-  cloud_id  = "<cloud_id>"
-  folder_id = "<folder_id>"
-  zone      = "<zone_name>"
+  token     = ""
+  cloud_id  = ""
+  folder_id = ""
+  zone      = "ru-central1-b"
 }
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "<vm_name>"
-  platform_id = "<name_of_cpu_platform>"
+resource "yandex_iam_service_account" "vmadmin" {
+  name        = "vmadmin"
+  description = "service account to manage VMs"
+}
+
+
+resource "yandex_resourcemanager_folder_iam_member" "vm-editor-role" {
+  folder_id = ""
+  role      = "editor"
+  member    = "serviceAccount:${yandex_iam_service_account.vmadmin.id}"
+}
+
+resource "yandex_compute_instance_group" "s-group1" {
+  name               = "react-vms"
+  service_account_id = yandex_iam_service_account.vmadmin.id
+  depends_on = [yandex_iam_service_account.vmadmin, yandex_resourcemanager_folder_iam_member.vm-editor-role]
+  instance_template {
+    name = "nginx-{instance.index}"
+    platform_id = "standard-v3"
+    resources {
+      cores         = 2
+      memory        = 1
+      core_fraction = 20
+    }
+    boot_disk {
+      initialize_params {
+        image_id = "fd82re2tpfl4chaupeuf"
+        size     = 10
+      }
+    }
+    network_interface {
+      network_id = yandex_vpc_network.s-network-1.id
+      subnet_ids = [yandex_vpc_subnet.s-subnet-1.id, yandex_vpc_subnet.s-subnet-2.id]
+      nat = true
+    }
+    metadata = {
+      ssh-keys = "ubuntu:${file("<ssh key path>")}"
+    }
+
+  }
+  load_balancer {
+    target_group_name = "balancer-target-group"
+  }
+  allocation_policy {
+    zones = ["ru-central1-b", "ru-central1-a"]
+  }
+  deploy_policy {
+    max_unavailable = 2
+    max_creating    = 2
+    max_expansion   = 2
+    max_deleting    = 2
+  }
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+
+}
+
+resource "yandex_compute_instance" "s-vm-1" {
+  name = "react-vm-1"
+  platform_id = "standard-v3"
 
   resources {
     cores  = 2
@@ -26,14 +86,14 @@ resource "yandex_compute_instance" "vm-1" {
 
   boot_disk {
     initialize_params {
-      image_id = "<image_id>"
+      image_id = "fd82re2tpfl4chaupeuf"
       size     = 10
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
+    subnet_id = yandex_vpc_subnet.s-subnet-1.id
+    nat = true
   }
 
   scheduling_policy {
@@ -41,25 +101,66 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("<path>/id_rsa.pub")}"
+    ssh-keys = "ubuntu:${file("<ssh key path>")}"
   }
 }
 
-resource "yandex_vpc_network" "network-1" {
+resource "yandex_vpc_network" "s-network-1" {
   name = "network1"
 }
 
-resource "yandex_vpc_subnet" "subnet-1" {
+resource "yandex_vpc_subnet" "s-subnet-1" {
   name           = "subnet1"
-  zone           = "<zone_name>"
-  network_id     = yandex_vpc_network.network-1.id
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.s-network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+resource "yandex_vpc_subnet" "s-subnet-2" {
+  name           = "subnet2"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.s-network-1.id
+  v4_cidr_blocks = ["192.168.11.0/24"]
 }
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+resource "yandex_lb_network_load_balancer" "s-balancer-1" {
+  name = "react-network-load-balancer"
+
+  listener {
+    name = "react-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.s-group1.load_balancer.0.target_group_id
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+      }
+    }
+  }
+}
+
+output "front-internal" {
+  value = yandex_compute_instance_group.s-group1.instances.*.network_interface.0.ip_address
+}
+
+output "front-external" {
+  value = yandex_compute_instance_group.s-group1.instances.*.network_interface.0.nat_ip_address
+}
+
+output "backend-internal" {
+  value = yandex_compute_instance.s-vm-1.network_interface.0.ip_address
+}
+
+output "backend-external" {
+  value = yandex_compute_instance.s-vm-1.network_interface.0.nat_ip_address
+}
+
+output "balancer-external" {
+  value = yandex_lb_network_load_balancer.s-balancer-1.listener
 }
