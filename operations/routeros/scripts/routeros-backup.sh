f12169db52fd470776d1d3ba49aff9672129ec24
Galonza Peter <p.galonza@evaron.ru>
2023-03-19 15:21:41 +0300

    add backup script

---
+++ operations/routeros/scripts/routeros-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+LOGIN=<user name>
+DATE=$(date +%Y-%m-%d_%k-%M-%S)
+
+declare -A devices
+devices[<name1>]=<ip address1>
+devices[<name2>]=<ip address2>
+devicesName=(<name1> <name2>)
+
+function backup() {
+  ssh $LOGIN@$1 "/system/backup/save dont-encrypt=yes name=$2-$DATE.backup"
+  scp $LOGIN@$1:~/$2-$DATE.backup .
+  ssh $LOGIN@$1 "/file remove $2-$DATE.backup"
+}
+
+for name in ${devicesName[@]}; do
+        backup ${devices[$name]} $name
+done
